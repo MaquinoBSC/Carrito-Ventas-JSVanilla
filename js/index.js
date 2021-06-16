088afed5844f8c58c0f8fd38b6c1b342f8ae6576
Martin Hernandez <martin.hernandez@tagwizz.com.mx>
2021-06-16 01:36:06 -0500

    add products to cart

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
-const CAR_PRODUCTS= "cartProductsId";
+const CART_PRODUCTS= "cartProductsId";
 
 document.addEventListener("DOMContentLoaded", ()=> {
     loadProducts();
+    loadProductsCart();
 })
 
 async function getProductsDB(){
@@ -37,7 +38,7 @@ async function loadProducts(){
                         <h5 class="card-title">${product.name}</h5>
                         <p class="card-text>${product.extraInfo}</p>
                         <p class="card-text>$${product.price} /Unidad</p>
-                        <button type="button" class="btn btn-primary btn-cart">Añadir al carrito</button>
+                        <button type="button" class="btn btn-primary btn-cart" onclick="addProductCart(${product.id})">Añadir al carrito</button>
                     </div>
                 </div>
             </div>
@@ -45,4 +46,108 @@ async function loadProducts(){
     })
 
     document.getElementsByClassName("products")[0].innerHTML= html;
+}
+
+function openCloseCart(){
+    const containerCart= document.getElementsByClassName('cart-products')[0]
+    
+    //Con classList obtenemos un array de todas las classes que tiene un elemento
+    containerCart.classList.forEach(item => {
+        if(item === 'hidden'){
+            //Usando classList tambien podemos add o remover clases de un elemento
+            containerCart.classList.remove('hidden');
+            containerCart.classList.add('active');
+        }
+        if(item === 'active'){
+            containerCart.classList.remove('active');
+            containerCart.classList.add('hidden');
+        }
+    })
+}
+
+function addProductCart(idProduct){
+    let productsID= [];
+    let localStorageItems= localStorage.getItem(CART_PRODUCTS);
+
+    if(localStorageItems === null){
+        productsID.push(idProduct);
+        localStorage.setItem(CART_PRODUCTS, productsID);
+    }
+    else{
+        productsID= localStorage.getItem(CART_PRODUCTS);
+        if(productsID.length > 0){
+            productsID += ","+idProduct
+        }
+        else{
+            productsID = idProduct;
+        }
+        localStorage.setItem(CART_PRODUCTS, productsID)
+    }
+    loadProductsCart();
+}
+
+
+async function loadProductsCart(){
+    const products= await getProductsDB();
+
+    const localStorageItems= localStorage.getItem(CART_PRODUCTS);
+    let html= '';
+
+    if(!localStorageItems){
+        html= `
+            <div class="cart-product empty">
+                <p>Carrito vacio</p>
+            </div>
+        `;
+    }
+    else{
+        //Convertimos el resultado del localStorage en un array
+        const idProductsSplit= localStorageItems.split(',');
+
+        //Eliminamos los id duplicados
+        const idProductsCart= Array.from(new Set(idProductsSplit));
+        
+        idProductsCart.forEach((id)=> {
+            console.log("Entramos");
+            products.forEach(product=> {
+                if(id == product.id){
+
+                    const quantity= countDuplicatesID(id, idProductsSplit);
+                    const totalPrice= product.price * quantity;
+                    html+= `
+                        <div class="cart-product">
+                            <img src="${product.image}" alt="${product.name}"/>
+                            <div class="cart-product-info">
+                                <span class="quantity">${quantity}</span>
+                                <p>${product.name}</p>
+                                <p>$${totalPrice.toFixed(2)} mxn</p>
+                                <p class="change-quantity">
+                                    <button>-</button>
+                                    <button>+</button>
+                                </p>
+                                <p class="cart-product-delete">
+                                    <button>Eliminar</button>
+                                </p>
+                            </div>
+                        </div>
+                    `;
+                }
+            });
+        });
+    }
+    
+
+    document.getElementsByClassName('cart-products')[0].innerHTML= html;
+}
+
+function countDuplicatesID(value, arrayIds){
+    let count= 0;
+
+    arrayIds.forEach(id => {
+        if(value == id){
+            count++;
+        }
+    });
+
+    return count;
 }
